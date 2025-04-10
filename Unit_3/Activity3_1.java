a2b6b01af619e649a5d3bd788f5d75abcf7deeea
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 13:13:06 +0200

    explaing the higher precedence on the number and oparators

---
+++ Unit_3/Activity3_1.java
@@ -0,0 +1,7 @@
+/*
+ * how will java evaluate the following expressions?
+ * 1. 1 + 2 - 3 * 4 / 5 ** java will start with multiplication followed by division, addition and subtraction.
+ * 2.  4 + 5 * 6 / 3 ** java will start with multiplication followed by division, addition and subtraction.
+ * 3. (1/2) * (4 +7) / 6 - 4 ** java will start by division followed by addition , multiplication and subtraction.
+ * 4. ((7-3) * 5) -(3*4)/2) ** java will start by (7-3) followed by (4+7) mult  and division and subtraction.
+ * */
