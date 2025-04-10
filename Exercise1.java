b8a70952079192a38403bbb11db407d5ba1b0164
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 10:54:52 +0200

    program that displays the message on the command line

---
+++ Exercise1.java
@@ -0,0 +1,14 @@
+/*
+ * Exercise 1
+ * Write a program that displays My name is....,
+ * i am a student at Mancosa.
+ * must be displayed on the command line.
+ * */
+
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("My name is: Ncayiyana Ziphozenkosi");
+        System.out.println("I am a student at Mancosa");
+    }
+}
