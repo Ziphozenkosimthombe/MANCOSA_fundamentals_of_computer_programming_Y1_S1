5692da8b3bd9b51ba4ed2239648e8ef026019462
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 11:33:03 +0200

    program that out put the message on the screen using the built-in function JOptionPane

---
+++ Unit_2/Activity2_1.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Activity2_1
+ * pragram  to output the message "Programming is great\n I love programming"
+ */
+
+public class Activity2_1{
+  public static void main(String args[]){
+    JOptionPane.showMessageDialog(null, "Programming is great\n I love programming");
+    System.exit(0);
+  }
+}
