7f808c2a024480371095485c9d0649f9beb77ac2
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 12:03:32 +0200

    program that calls method JOptionPane as many times as i want

---
+++ Unit_2/Activity2_3.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+/*
+ * program that's call method showMessageDialog many times.
+ * */
+
+public class Activity2_3{
+  public static void main(String args[]){
+    JOptionPane.showMessageDialog(null, "Hello Everyone");
+    JOptionPane.showMessageDialog(null, "How are you?"); 
+    JOptionPane.showMessageDialog(null, "It's good to meet you.");
+    System.exit(0);
+  }
+}
