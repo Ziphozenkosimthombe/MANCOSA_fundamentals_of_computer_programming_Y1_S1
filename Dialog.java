263018ffe5064fa8108d7e0347c6f81874e23326
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 10:47:15 +0200

    pragram that show the message on the screen using the JOptionPane

---
+++ Dialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+
+public class Dialog{
+  public static void main(String args[]){
+    JOptionPane.showMessageDialog(null, "This is my message");
+    System.exit(0);
+  }
+}
