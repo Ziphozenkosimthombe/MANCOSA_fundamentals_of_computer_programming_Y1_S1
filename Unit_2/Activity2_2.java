89f0100938c2fe6c28c6cfcb22408b6978adccad
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-10 11:54:42 +0200

    program to output the half of the triangle

---
+++ Unit_2/Activity2_2.java
@@ -0,0 +1,20 @@
+//import javax.swing.JOptionPane;
+
+
+/*
+ * program called Triangle to output the triangle in a dialog box.
+ * */
+
+public class Activity2_2{
+  public static void main(String args[]){
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < i; j++){
+        //JOptionPane.showMessageDialog(null, "*");
+      System.out.print("*");
+      }
+      //JOptionPane.showMessageDialog(null, "");
+      System.out.println("");
+    }
+    //System.exit(0);
+  }
+}
