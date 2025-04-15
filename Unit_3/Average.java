d2ca39f8a2fa66fbf15233a4d71b037be24575b6
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-04-15 23:51:47 +0200

    calculating the average of the numbers

---
+++ Unit_3/Average.java
@@ -0,0 +1,10 @@
+public class Average{
+  public static void main(String[] args){
+    int num1 = 0;
+    int num2 = 10;
+    int num3 = 4;
+    int average;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
