b26c7e87220ea11f0ef1607155fa4929bd78a3e5
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-13 12:41:30 +0200

    program that print the text 5 times

---
+++ Unit_5/Text.java
@@ -0,0 +1,13 @@
+/*
+ * program that display text 5 times using the while loop statementse
+ * */
+
+public class Text{
+  public static void main(String[] args){
+    int count = 1;
+    while(count <= 5){
+      System.out.println(count + ".This is an example of a while loop");
+      count++;
+    }
+  }
+}
