234af9fdc7234ac018346d5736b6b3524e0d4246
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-13 12:36:40 +0200

    program that used the while loop to count the sheep

---
+++ Unit_5/Activity5_2.java
@@ -0,0 +1,17 @@
+
+/*
+ * program that print "I have 6 sheep"
+ * using the while loop statements
+ * */
+
+
+
+public class Activity5_2{
+  public static void main(String[] args){
+    int count = 1;
+    while(count <= 6){
+      System.out.println("I have " + count + " sheep");
+      count++;
+    }
+  }
+}
