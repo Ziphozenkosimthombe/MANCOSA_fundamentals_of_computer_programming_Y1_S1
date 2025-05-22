0acea2149678a03bd8da69f696936c397bdc8f5c
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-22 23:21:00 +0200

    program that print hello java 20 times using for loop

---
+++ Unit_6/HelloJava.java
@@ -0,0 +1,12 @@
+/*
+ * program that print hello java 20times using for loop
+ * */
+
+
+public class HelloJava{
+  public static void main(String[] args){
+    for (int i = 1; i <= 20; i++){
+      System.out.println("Hello Java");
+    }
+  }
+}
