291ede8ef76005165b3aa1c8b316ca02ca96c56c
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-22 23:29:28 +0200

    program that count backward from 100 to 0

---
+++ Unit_6/CountBackward.java
@@ -0,0 +1,13 @@
+/*
+ * program that count backward from 100 to 0
+ * */
+
+
+public class CountBackward{
+  public static void main(String[] args){
+    int i = 100;
+    for(; i >= 0; i--){
+      System.out.println(i);
+    }
+  }
+}
