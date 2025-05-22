be9e84eb2add88046ea5a01c66110b9072389f4b
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-22 22:41:40 +0200

    program that output Today is Sunday five times using do while loop

---
+++ Unit_6/Sunday.java
@@ -0,0 +1,13 @@
+/*
+ * program that output the string Today is Sunday five times
+ * */
+
+public class Sunday{
+  public static void main(String[] args){
+    int i = 1;
+    do{
+      System.out.println("Today is Sunday");
+      i++;
+    }while(i <= 5);
+  }
+}
