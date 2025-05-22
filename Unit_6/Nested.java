104f6184d6a7e0d62103041024f07c4e276b871b
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-22 23:43:16 +0200

    nested for llop

---
+++ Unit_6/Nested.java
@@ -0,0 +1,12 @@
+
+
+public class Nested {
+  public static void main(String[] args) {
+    for(int i = 0; i <= 5; i++){
+      for(int k = 0; k < i; k++){
+        System.out.print("@");
+      }
+      System.out.println();
+    }
+  }
+}
