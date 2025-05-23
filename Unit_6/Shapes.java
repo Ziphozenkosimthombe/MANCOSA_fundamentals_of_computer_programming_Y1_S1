084f7723dd7fd487f70ed99ae1911d13b89d5711
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-05-23 22:30:49 +0200

    nested for loop that prints out the shapes

---
+++ Unit_6/Shapes.java
@@ -0,0 +1,10 @@
+public class Shapes {
+  public static void main(String[] args) {
+    for(int i = 1; i <= 4; i++){
+      for(int k = 1; k <= 4; k++){
+        System.out.print("&");
+      }
+      System.out.println();
+    }
+  }
+}
